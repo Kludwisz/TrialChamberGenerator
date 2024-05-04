29fb9232ed48c13f6f58ea7c30bc1db988908963
kludw <kludwisz@gmail.com>
2024-05-04 21:26:12 +0200

    update to 24w18a

--- src/main/java/kludwisz/generator/TrialChambersGenerator.java
+++ src/main/java/kludwisz/generator/TrialChambersGenerator.java
@@ -33,7 +33,7 @@ public class TrialChambersGenerator {
     private static final int MAX_DIST = 116; // max distance from start piece
     private static final int MAX_DEPTH = 20; // defined as "size" in the client jar
     private static final int EMPTY_PIECE_ID = 170;
-    private static final int EMPTY_POOL_ID = 44;
+    private static final int EMPTY_POOL_ID = 45;
     private static final int START_POOL_ID = 7; // trial_chambers/chamber/end
     private static final ArrayList<Integer> START_TEMPLATES = getTemplatesFromPool(TrialChambersPools.get(START_POOL_ID));
 
